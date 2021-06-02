ce8dad4d8c660f86c36fd18251dc3831214cb11b
Josh <josh.kutschera@gmail.com>
2021-06-02 19:21:36 +0200

    test

---
+++ Items.java
@@ -0,0 +1,27 @@
+public class Items
+{
+    private String Name;
+    private String Description;
+    public Items(String name, String description)
+    {
+        setName(name);
+        setDescription(description);
+    }
+    private void setName(String name)
+    {
+        Name = name;
+    }
+    private void setDescription(String description)
+    {
+        Description = description;
+    }
+    public String getName()
+    {
+        return Name;
+    }
+    public String getDescription()
+    {
+        return Description;
+    }
+
+}
