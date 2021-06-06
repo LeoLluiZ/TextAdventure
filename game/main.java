e15e28226cee80f258f76749bd299417e211bfa8
LeoLluiZ <85246893+LeoLluiZ@users.noreply.github.com>
2021-06-06 12:47:27 +0200

    Update main.java

--- game/main.java
+++ game/main.java
@@ -1,4 +1,7 @@
 package game;
+
+import java.util.Scanner;
+
 public class main {
     private int y=1;
     private int x=3;
@@ -13,9 +16,18 @@ public class main {
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
         map.printMap();
+        input();
+    }
+    private int getY()
+    {
+        return y;
+    }
+    private int getX()
+    {
+        return x;
     }
     public void goNorth(){
-        if(map.getMapObject(x,y+1)!=null&&!map.getMapObject(x, y+1).getNameObject().contains("Door"))
+        if(map.getMapObject(x,y+1)!=null&&!map.getMapObject(x, y--+1).getNameObject().contains("Door"))
         {
             y++;
         }
@@ -80,5 +92,64 @@ public class main {
             }
         }
         return false;
+    }
+    public void  printInventory()
+    {
+        for(int i=0;i<inventory.length;i++)
+        {
+            if(inventory[i]!=null)
+            {
+                System.out.println(-----------------------------------------);
+                System.out.println("| "+inventory[i].getNameItem()+"                                      "+"|");
+                //spaces have to be
+
+            }
+        }
+        System.out.println(-----------------------------------------);
+    }
+    public void help()
+    {
+        System.out.println("Method to help player if needed."); // nur ein Befehl auf einmal, groß-klein schreibung ist egal
+    }
+    public void input()
+    {
+        Scanner keyboard=new Scanner(System.in);
+        String input=keyboard.nextLine();
+        input=input.toLowerCase();
+        for(int i=0;i<input.length();i++)
+        {
+            //text recognition for movement methods
+            switch(input.split(" ")[i]){
+                case "go": case "move":
+                    for(int x=0;x<input.length();x++)
+                    {
+                        switch(input.split(" ")[i]){
+                            case "north": case "up": case "upwards":
+                                this.goNorth();
+                                return;
+                            case "east": case "right":
+                                this.goEast();
+                                return;
+                            case "south": case "down": case "downwards":
+                                this.goSouth();
+                                return;
+                            case "west": case "left":
+                                this.goWest();
+                                return;
+                        }
+
+                        System.out.println("Input couldn't be recognized");
+                        return;
+                    }
+                    // text recognition for help
+                case "man": case "manual": case "help":
+                    this.help();
+                    return;
+                    //Item recognition
+                case "inventory": case "bag":
+                    this.printInventory();
+                    break;
+            }
+        }
     }
 }
