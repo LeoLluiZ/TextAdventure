5c63cccb1739b8cd082baebfa485724ca24e875e
Josh <josh.kutschera@gmail.com>
2021-06-09 08:36:30 +0200

    Updated Map

--- game/Actions.java
+++ game/Actions.java
@@ -3,21 +3,27 @@ package game;
 import java.util.Scanner;
 
 public class Actions {
+    private int moves=0;
     private int y=1;
     private int x=3;
     private final Map map=new Map();
     private Items[] inventory=new Items[100];
+    public int getMoves(){
+        return moves;
+    }
     //movement methods
     public void goNorth(){
         if(map.getMapObject(x,y+1)!=null&&!map.getMapObject(x, y--+1).getNameObject().contains("Door"))
         {
             y++;
+            moves++;
         }
         else
         {
             if(map.getMapObject(x,y+1).getNameObject().equals("Door")||searchInventory("Keycard for "+map.getMapObject(x,y+1).getNameObject()))
             {
                 y+=2;
+                moves++;
             }
         }
         System.out.println(map.getMapObject(x,y).getDescriptionObject());
@@ -26,12 +32,14 @@ public class Actions {
         if(map.getMapObject(x,y-1)!=null&&!map.getMapObject(x,y-1).getNameObject().contains("Door"))
         {
             y--;
+            moves++;
         }
         else
         {
             if(map.getMapObject(x,y-1).getNameObject().equals("Door")||searchInventory("Keycard for "+map.getMapObject(x,y-1).getNameObject()))
             {
                 y-=2;
+                moves++;
             }
         }
         System.out.println(map.getMapObject(x,y).getDescriptionObject());
@@ -40,12 +48,14 @@ public class Actions {
         if(map.getMapObject(x+1,y)!=null&&!map.getMapObject(x+1,y).getNameObject().contains("Door"))
         {
             x++;
+            moves++;
         }
         else
         {
             if(map.getMapObject(x+1,y).getNameObject().equals("Door")||searchInventory("Keycard for "+map.getMapObject(x+1,y).getNameObject()))
             {
                 x+=2;
+                moves++;
             }
         }
         System.out.println(map.getMapObject(x,y).getDescriptionObject());
@@ -54,12 +64,14 @@ public class Actions {
         if(map.getMapObject(x-1,y)!=null&&!map.getMapObject(x-1,y).getNameObject().contains("Door"))
         {
             x--;
+            moves++;
         }
         else
         {
             if(map.getMapObject(x-1,y).getNameObject().equals("Door")||searchInventory("Keycard for "+map.getMapObject(x-1,y).getNameObject()))
             {
                 x-=2;
+                moves++;
             }
         }
         System.out.println(map.getMapObject(x,y).getDescriptionObject());
@@ -94,7 +106,7 @@ public class Actions {
                 break;
             }
         }
-        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("---------------------------------------------------------------------------------\n");
     }
     //help method
     public void help()
--- game/main.java
+++ game/main.java
@@ -13,9 +13,17 @@ public class main {
         System.out.println("- - - - - - - - - - - - - - - - - -"+"\033[36m"+" Intro test"+"\033[0m"+"- - - - - - - - - - - - - - - - - -");
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-        while(true) {
+        while(actions.getMoves()<2) {
             map.printMap();
             actions.input();
+        }
+        if(actions.getMoves()<2)
+        {
+            System.out.println("Game Over");
+        }
+        else
+        {
+            System.out.println("Mission complete");
         }
     }
 }
