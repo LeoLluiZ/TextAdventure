507682bb721053b0ba61b29602fb331058f18e8a
josh <josh.kutschera@gmail.com>
2021-06-18 12:54:43 +0200

    reworked map

--- game/main.java
+++ game/main.java
@@ -16,8 +16,6 @@ public class main {
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
         while(actions.getMoves()<end/*&&actions.getEscapePods()*/) {
-            System.out.println(actions.getX());
-            System.out.println(actions.getY());
             map.printMap(actions.getX(), actions.getY());
             actions.input();
         }
